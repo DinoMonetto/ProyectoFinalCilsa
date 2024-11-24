d2f3a09facf1ea73c3d062da77f59bae94cef2ab
Luz255181 <mira_luz_explo@hotmail.com>
2024-11-24 12:25:02 -0300

    solucionado el problema de porta

--- backend/app.js
+++ backend/app.js
@@ -8,14 +8,14 @@ import dotenv from 'dotenv'; // Importa dotenv para manejar variables de entorno
 dotenv.config(); // Configura dotenv.
 
 const app = express(); // Crea la aplicación Express.
-const PORT = process.env.PORT || 5000; // Toma el puerto del archivo .env o usa el puerto 5000 por defecto.
+const PORT = process.env.PORT_APP || 5000; // Toma el puerto del archivo .env o usa el puerto 5000 por defecto.
 
 connectDB(); // Conecta con la base de datos.
 
 app.use(cors({// Habilita CORS para todas las solicitudes.
-  origin: 'http://localhost:3000'  // Esto permite solicitudes solo desde este puerto
+    origin: 'http://localhost:3000'  // Esto permite solicitudes solo desde este puerto
 }));
-; 
+;
 app.use(bodyParser.json()); // Configura body-parser para procesar datos JSON.
 
 app.use('/api/tasks', taskRoutes); // Configura las rutas para las tareas.
@@ -24,7 +24,7 @@ app.get('/', (req, res) => res.send('API funcionando')); // Ruta base de prueba.
 
 // Inicia el servidor y escucha en el puerto especificado
 app.listen(PORT, () => {
-  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+    console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
 
 export default app; // Exporta la aplicación para iniciar el servidor.
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import app from './app.js'; // Importa la aplicación Express.
 
-const PORT = process.env.PORT || 5000; // Define el puerto en el que se ejecutará el servidor.
+const PORT = process.env.PORT_SERVER || 5000; // Define el puerto en el que se ejecutará el servidor.
 
 app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`)); // Inicia el servidor y muestra un mensaje.
